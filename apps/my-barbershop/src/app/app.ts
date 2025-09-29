21e0800e041903d7624813292b9b98383460a21c
Lucas Freitas <lucasfrittas@gmail.com>
2025-09-29 11:46:36 -0300

    ✨ feat: add basic structure and styles

--- apps/my-barbershop/src/app/app.ts
+++ apps/my-barbershop/src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule],
   selector: 'mb-root',
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+  templateUrl: './app.component.html',
 })
 export class App {
   protected title = 'my-barbershop';
---
+++ apps/my-barbershop/src/app/core/layout/auth/auth/auth.layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mb-auth',
+  imports: [],
+  templateUrl: './auth.layout.html',
+  styleUrl: './auth.layout.scss',
+})
+export class AuthLayout {}
